bfddb7f0eecf8563ab4f349f2b767c8f1cee6e11
Hugo Cruz <hugocruzlfc@gmail.com>
2025-09-30 09:42:54 +0200

    feat: update current dependecies

--- template/src/app/page.tsx
+++ template/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center space-y-5 p-10">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        Nextjs (15.3.0) + Shadcn UI + Tailwindcss V4 + Typescript + Eslint 9 +
+        Nextjs (15.15.0) + Shadcn UI + Tailwindcss V4 + Typescript + Eslint 9 +
         Prettier + Husky + Lint-staged + Commitlint + Commitizen
       </h1>
       <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
