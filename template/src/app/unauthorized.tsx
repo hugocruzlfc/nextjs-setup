f92529d62e9854bcc7752d6c391ebc3e2e1927c8
Hugo Cruz <hugocruzlfc@gmail.com>
2025-09-30 09:46:25 +0200

    perf: add new features for next current version

---
+++ template/src/app/unauthorized.tsx
@@ -0,0 +1,21 @@
+// import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+
+export default function UnauthorizedPage() {
+  return (
+    <main className="flex grow items-center justify-center px-4 text-center">
+      <div className="space-y-6">
+        <div className="space-y-2">
+          <h1 className="text-2xl font-semibold">401 - Unauthorized</h1>
+          <p className="text-muted-foreground">Please sign in to continue.</p>
+        </div>
+        <div>
+          {/* // If you have a sign-in page, uncomment the code below */}
+          {/* <Button asChild>
+            <Link href="/sign-in">Sign in</Link>
+          </Button> */}
+        </div>
+      </div>
+    </main>
+  );
+}
